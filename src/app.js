b1f2dc67cae860a4b7595c55a6c9cd9cbc3c0cc4
gunesh <gunesh26@gmail.com>
2022-05-18 20:55:42 +0530

    1 file modified

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Captcha from './captcha'
 
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <Captcha />
     </div>
   );
 }
