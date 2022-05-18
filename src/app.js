f11572b43f46b9e4b73fb4787cb35145b1bd8cc0
gunesh <gunesh26@gmail.com>
2022-05-18 11:49:25 +0530

    1 file modified

--- src/app.js
+++ src/app.js
@@ -1,20 +1,28 @@
-import React,{useReducer} from 'react';
+import React, { useReducer } from 'react';
 
-function userReducer(state={item:[],loader:false},action){
-  switch(action.type){
-    case "ADD":
-      return {...state}
-      default:
-        return state
+function userReducer(state = { item: [], loader: false }, action) {
+  console.log(state, action);
+  switch (action.type) {
+    case 'ADD':
+      return { ...state };
+    default:
+      return state;
   }
 }
 export default function App() {
-  const [state,dispatch] = useReducer(userReducer,[])
-  console.log(state)
+  const [state, dispatch] = useReducer(userReducer, {});
+  console.log(state);
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <button
+        onClick={() => {
+          dispatch({ type: 'ADD' });
+        }}
+      >
+        Add
+      </button>
     </div>
   );
 }
