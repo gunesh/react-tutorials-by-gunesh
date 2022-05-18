afdfb4c1fc51894e9da331784fbb25b346b6a2a1
gunesh <gunesh26@gmail.com>
2022-05-18 11:43:36 +0530

    1 file modified, 1 added

--- src/app.js
+++ src/app.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React,{useReducer} from 'react';
 
+function userReducer(state={item:[],loader:false},action){
+  switch(action.type){
+    case "ADD":
+      return {...state}
+      default:
+        return state
+  }
+}
 export default function App() {
+  const [state,dispatch] = useReducer(userReducer,[])
+  console.log(state)
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
