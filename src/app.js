15c7fe67fd77e20bdb3b90af5d94feb0bb1e5ced
gunesh <gunesh26@gmail.com>
2022-05-28 13:48:39 +0530

    1 file modified

--- src/app.js
+++ src/app.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import {Captcha,MyCaptcha} from './captcha'
-import Hello from './mobx/Hello'
+import React, { Fragment } from 'react';
+import { Captcha, MyCaptcha } from './captcha';
+import Hello from './mobx/Hello';
+import { observer } from 'mobx-react';
+import { useMyStore } from './mobx/hooks';
 
-export default function App() {
+function App() {
+  const myStore = useMyStore();
+  const list = myStore.myOptions;
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      {list.map((item, index) => {
+        return (
+          <Fragment key={`${item.a}-${index}`}>
+            {`- ${item.a}- `} <br />
+          </Fragment>
+        );
+      })}
+      <br />
+      Total : {list.length}
       <Hello />
     </div>
   );
 }
+
+export default observer(App);
