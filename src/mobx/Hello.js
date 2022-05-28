097579434c76dd760490cb4e5fad1cfa6743cd9c
gunesh <gunesh26@gmail.com>
2022-05-28 12:13:35 +0530

    1 file modified

--- src/mobx/Hello.js
+++ src/mobx/Hello.js
@@ -1,8 +1,12 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { useMyStore } from '../hooks';
 
 const Hello = (prop) => {
   const myStore = useMyStore();
+
+  useEffect(() => {
+    console.log('ABC');
+  }, []);
 
   return <Fragment>Hello</Fragment>;
 };
