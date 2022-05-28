debee5091f6d1ee3c37ea764beb9dda1af66d2ab
gunesh <gunesh26@gmail.com>
2022-05-28 14:13:15 +0530

    1 file modified, 1 added

--- src/app.js
+++ src/app.js
@@ -1,28 +1,15 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Captcha, MyCaptcha } from './captcha';
 import Hello from './mobx/Hello';
-import { observer } from 'mobx-react';
-import { useMyStore } from './mobx/hooks';
 
 function App() {
-  const myStore = useMyStore();
-  const list = myStore.myOptions;
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
-      {list.map((item, index) => {
-        return (
-          <Fragment key={`${item.a}-${index}`}>
-            {`- ${item.a}- `} <br />
-          </Fragment>
-        );
-      })}
-      <br />
-      Total : {list.length} <br />
       <Hello />
     </div>
   );
 }
 
-export default observer(App);
+export default App;
---
+++ src/mobx/App2.js
@@ -0,0 +1,28 @@
+import React, { Fragment } from 'react';
+import { Captcha, MyCaptcha } from './captcha';
+import Hello from './mobx/Hello';
+import { observer } from 'mobx-react';
+import { useMyStore } from './mobx/hooks';
+
+function App() {
+  const myStore = useMyStore();
+  const list = myStore.myOptions;
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+      {list.map((item, index) => {
+        return (
+          <Fragment key={`${item.a}-${index}`}>
+            {`- ${item.a}- `} <br />
+          </Fragment>
+        );
+      })}
+      <br />
+      Total : {list.length} <br />
+      <Hello />
+    </div>
+  );
+}
+
+export default observer(App);
