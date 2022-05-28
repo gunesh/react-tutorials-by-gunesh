d0da51108b2f6cf240e89a31e2761dfa701b75e0
gunesh <gunesh26@gmail.com>
2022-05-28 12:21:45 +0530

    1 file modified

--- src/mobx/Hello.js
+++ src/mobx/Hello.js
@@ -4,11 +4,11 @@ import { useMyStore } from './hooks';
 const Hello = (prop) => {
   const myStore = useMyStore();
   const list = myStore.myOptions;
-  useEffect(() => {
-    console.log('Before Add', myStore.myOptions);
-    myStore.addEle();
-    console.log('After Add', myStore.myOptions);
-  }, []);
+  // useEffect(() => {
+  //   console.log('Before Add', myStore.myOptions);
+  //   myStore.addEle();
+  //   console.log('After Add', myStore.myOptions);
+  // }, []);
 
   return (
     <Fragment>
