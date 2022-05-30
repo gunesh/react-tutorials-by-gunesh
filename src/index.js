1be53152a2b87999dfa887944f902f1e0a995fc8
gunesh <gunesh26@gmail.com>
2022-05-30 12:40:14 +0530

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './app';
+// import React, { StrictMode } from 'react';
+// import { createRoot } from 'react-dom/client';
+// import App from './app';
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
+// const rootElement = document.getElementById('root');
+// const root = createRoot(rootElement);
 
-root.render(<App />);
+// root.render(<App />);
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>Hello, from Guru99 Tutorials!</h1>,
+  document.getElementById('root')
+);
