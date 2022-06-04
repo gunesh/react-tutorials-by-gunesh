a0ebde380cd73eaae0f663168effa63ed99f8235
gunesh <gunesh26@gmail.com>
2022-06-04 15:11:12 +0530

    1 file modified

--- src/example.js
+++ src/example.js
@@ -43,7 +43,7 @@ function Curring() {
   })(1)(2)(3)(4);
 }
 
-function Spead(user) {
+function Spead() {
   console.log('=================== Spread Example=================');
   let arr = [1, 2, 3];
   let arr2 = [4, 5, 6];
@@ -51,12 +51,16 @@ function Spead(user) {
   console.log('Without Spread:', [arr, arr2]);
 }
 
-function Rest(user) {
-  console.log('=================== Spread Example=================');
-  let arr = [1, 2, 3];
-  let arr2 = [4, 5, 6];
-  console.log('With Spread:', [...arr, ...arr2]);
-  console.log('Without Spread:', [arr, arr2]);
+function Rest() {
+  console.log('=================== Rest Example=================');
+  function restExample(...restparams) {
+    console.log('With Rest:', restparams);
+  }
+  function withoutRestExample(a, b, c) {
+    console.log('Param:', a, b, c);
+  }
+  restExample(1, 2, 3, 5, 6, 7); // We can add/remove arguments any times
+  withoutRestExample(4, 5, 6);
 }
 
 function Test() {
@@ -64,4 +68,4 @@ function Test() {
   console.log(arguments);
 }
 
-export { Test, Closure, Curring, Spead,Rest };
+export { Test, Closure, Curring, Spead, Rest };
