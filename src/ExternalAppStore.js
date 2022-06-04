d68f8464678f3a6a221014d26da45608f78bb6af
gunesh <gunesh26@gmail.com>
2022-06-04 11:51:36 +0530

    1 file modified, 5 added, 5 deleted

--- src/ExternalAppStore.js
+++ src/ExternalAppStore.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback, useMemo } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   Container,
@@ -8,22 +8,12 @@ import {
   NavbarBrand,
   NavbarToggler,
   Collapse,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
   NavbarText,
 } from 'reactstrap';
-import Search from './search';
-import HomePage from './list';
-import Add from './add';
-import { MemoizeExamples } from './Examples/memoexample';
-import { userActions } from './red/_actions';
-import { Provider } from 'react-redux';
-import { store } from './red/store';
+import Search from './Test/search';
+import HomePage from './Test/list';
+import Add from './Test/add';
+import { userActions } from './redux/_actions';
 
 function ExternalAppStore() {
   const users = useSelector((state) => state.users);
@@ -68,75 +58,49 @@ function ExternalAppStore() {
   }
 
   return (
-    <Provider store={store}>
-      <Container>
-        <Row>
-          <Col>
-            <div>
-              <Navbar color="light" expand="md" light>
-                <NavbarBrand href="/">reactstrap</NavbarBrand>
-                <NavbarToggler
-                  onClick={() => {
-                    setToggle(!toggle);
-                  }}
-                />
-                <Collapse isOpen={toggle} navbar={true}>
-                  {/* <NavItem>
-                    <NavLink href="/components/">Components</NavLink>
-                  </NavItem> */}
-                  {/* <NavItem>
-                    <NavLink href="https://github.com/reactstrap/reactstrap">
-                      GitHub
-                    </NavLink>
-                  </NavItem>
-                  <UncontrolledDropdown inNavbar nav>
-                    <DropdownToggle caret nav>
-                      Options
-                    </DropdownToggle>
-                    <DropdownMenu >
-                      <DropdownItem>Option 1</DropdownItem>
-                      <DropdownItem>Option 2</DropdownItem>
-                      <DropdownItem divider />
-                      <DropdownItem>Reset</DropdownItem>
-                    </DropdownMenu>
-                  </UncontrolledDropdown> */}
-                  <NavbarText>Simple Text</NavbarText>
-                </Collapse>
-              </Navbar>
-            </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Search
-              callback={debounceExample}
-              onChange={onChangeSearch}
-              delay={3000}
-            />
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Add handleAdd={handleAdd} /> <br />
-          </Col>
-        </Row>
-        {/* <Row>
-          <Col>
-            <MemoizeExamples />
-          </Col>
-        </Row> */}
+    <Container>
+      <Row>
+        <Col>
+          <div>
+            <Navbar color="light" expand="md" light>
+              <NavbarBrand href="/">reactstrap</NavbarBrand>
+              <NavbarToggler
+                onClick={() => {
+                  setToggle(!toggle);
+                }}
+              />
+              <Collapse isOpen={toggle} navbar={true}>
+                <NavbarText>Simple Text</NavbarText>
+              </Collapse>
+            </Navbar>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Search
+            callback={debounceExample}
+            onChange={onChangeSearch}
+            delay={3000}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Add handleAdd={handleAdd} /> <br />
+        </Col>
+      </Row>
 
-        <Row>
-          <Col>
-            <HomePage
-              handleReload={handleReload}
-              handleDeleteUser={handleDeleteUser}
-              users={userlist}
-            />
-          </Col>
-        </Row>
-      </Container>
-    </Provider>
+      <Row>
+        <Col>
+          <HomePage
+            handleReload={handleReload}
+            handleDeleteUser={handleDeleteUser}
+            users={userlist}
+          />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
