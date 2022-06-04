1f98f080c0b8b1b35cea7a4892af74ef3104fec0
gunesh <gunesh26@gmail.com>
2022-06-04 12:17:35 +0530

    1 file modified

--- src/app.js
+++ src/app.js
@@ -1,12 +1,53 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
-function App() {
+const initState = { item: [], loader: false };
+function userReducer(state, action) {
+  switch (action.type) {
+    case 'ADD':
+      return { ...state, item: [...state.item, action.payload] };
+    case 'REMOVE':
+      return {
+        ...state,
+        item: state.item.filter((obj) => {
+          return obj.id !== action.payload;
+        }),
+      };
+    default:
+      return state;
+  }
+}
+
+export default function App() {
+  const [state, dispatch] = useReducer(userReducer, initState);
+  const { item, loader } = state;
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <button
+        onClick={() => {
+          dispatch({ type: 'ADD', payload: { id: uuidv4() } });
+        }}
+      >
+        Add
+      </button>
+      <br />
+      {item.map((item) => {
+        return (
+          <>
+            <b
+              key={item.id}
+              onClick={() => {
+                dispatch({ type: 'REMOVE', payload: item.id });
+              }}
+            >
+              {item.id}
+            </b>{' '}
+            <br />
+          </>
+        );
+      })}
     </div>
   );
 }
-
-export default App;
