7358962713c3d66e571658883de6dbc3d4a93fc4
gunesh <gunesh26@gmail.com>
2022-06-04 12:32:05 +0530

    2 files modified, 22 deleted

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import React, { useReducer } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 const initState = { item: [], loader: false };
-function userReducer(state, action) {
+const myReducer = (state, action)=> {
   switch (action.type) {
     case 'ADD':
       return { ...state, item: [...state.item, action.payload] };
@@ -19,35 +19,38 @@ function userReducer(state, action) {
 }
 
 export default function App() {
-  const [state, dispatch] = useReducer(userReducer, initState);
+  const [state, dispatch] = useReducer(myReducer, initState);
   const { item, loader } = state;
   return (
     <div>
       <h1>useReducer Example</h1>
-      
+
       <button
         onClick={() => {
+          console.log(state)
           dispatch({ type: 'ADD', payload: { id: uuidv4() } });
         }}
       >
         Add
       </button>
       <br />
-      {item.map((item) => {
-        return (
-          <>
-            <b
-              key={item.id}
-              onClick={() => {
-                dispatch({ type: 'REMOVE', payload: item.id });
-              }}
-            >
-              {item.id}
-            </b>{' '}
-            <br />
-          </>
-        );
-      })}
+      <ul>
+        {item.map((item) => {
+          return (
+            <li key={item.id}>
+              <b
+                key={item.id}
+                onClick={() => {
+                  dispatch({ type: 'REMOVE', payload: item.id });
+                }}
+              >
+                {item.id}
+              </b>{' '}
+              <br />
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
