a68c926ac3d97c33a22301fc9d3478451c0a31df
gunesh <gunesh26@gmail.com>
2022-06-04 11:38:53 +0530

    1 file modified

--- src/onelinesetup.js
+++ src/onelinesetup.js
@@ -51,9 +51,8 @@ function App() {
   };
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-
+      <h1>React Redux Test</h1>
+    
       <input
         type="text"
         name="name"
