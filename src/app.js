072dfa7f1e677345b228842a1eae5f80c6689ffc
gunesh <gunesh26@gmail.com>
2022-06-04 11:12:01 +0530

    1 file modified

--- src/app.js
+++ src/app.js
@@ -2,13 +2,12 @@ import React from 'react';
 import App2 from './mobx/App2';
 import Table from './mobx/table';
 
-
 function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
-     <App2 />
+      <App2 />
       <Table />
     </div>
   );
