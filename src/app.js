41295923a5eae5dd71602cad1c64f325fb8222ae
gunesh <gunesh26@gmail.com>
2022-06-04 12:01:39 +0530

    2 files modified, 16 deleted

--- src/app.js
+++ src/app.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import { Captcha, MyCaptcha } from './captcha';
-import Hello from './mobx/Hello';
-import App2 from './mobx/App2';
-import Table from './mobx/table';
-import GoogleChart from './Charts/GoogleChart';
-import { MapContainer } from './GoogleMap';
-import { TableExample } from './Tables';
 
 function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
-      <App2 />
     </div>
   );
 }
