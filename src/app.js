c99bf53d7c577ea1b1883534aff06fb1fb239fcc
gunesh <gunesh26@gmail.com>
2022-06-05 15:42:02 +0530

    2 files modified

--- src/app.js
+++ src/app.js
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { memo } from 'react';
 
-
-function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    
-    </div>
-  );
+class WithPureClassComponent extends React.PureComponent {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    {
+      console.log('Pure Class component Load');
+    }
+    return <h1>Hello == {this.props.name}</h1>;
+  }
 }
 
-export default App;
+const WithPureFunctionalComponent = memo((props) => {
+  return (
+    <div>
+      {console.log('Pure Functional component Load')}
+      <h1>Hello == {props.name}</h1>
+    </div>
+  );
+});
+
+class WithoutPureClassComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    {
+      console.log('Class component Load');
+    }
+    return <h1>Hello == {this.props.name}</h1>;
+  }
+}
+
+const WithoutPureFunctionalComponent = (props) => {
+  return (
+    <div>
+      {console.log('Functional component Load')}
+      <h1>Hello == {props.name}</h1>
+    </div>
+  );
+};
+
+export {
+  WithPureFunctionalComponent,
+  WithPureClassComponent,
+  WithoutPureClassComponent,
+  WithoutPureFunctionalComponent,
+};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './app';
+import {
+  WithPureFunctionalComponent,
+  WithPureClassComponent,
+  WithoutPureClassComponent,
+  WithoutPureFunctionalComponent,
+} from './app';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
-root.render(<App />);
+root.render(
+  <div>
+    <WithPureFunctionalComponent name="gunesh" />
+    <WithPureClassComponent name="gunesh" />
+
+    <WithoutPureClassComponent name="gunesh" />
+    <WithoutPureFunctionalComponent name="gunesh" />
+  </div>
+);
