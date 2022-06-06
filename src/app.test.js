3460e28b5e81894b8c76215c2f114d127bd08c07
gunesh <gunesh26@gmail.com>
2022-06-06 22:17:07 +0530

    1 file added

---
+++ src/app.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './app';
+describe("ComponentName", () => {
+  it("should render my component", () => {
+    const wrapper = shallow(<App />);
+  });
+});
