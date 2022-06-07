5a4659e2b12f262a38982802b0c576ca48661b7f
gunesh <gunesh26@gmail.com>
2022-06-07 21:07:40 +0530

    1 file modified, 4 added

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import StockList from './StockList';
 
 function Hoc(HocComponent) {
   return class extends Component {
@@ -15,10 +16,70 @@ function Hoc(HocComponent) {
 function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>HOC Example!</h1>
+      <StockList></StockList>
     </div>
   );
 }
 
 export default Hoc(App);
+
+import React, { Component } from 'react';
+import StockList from './StockList';
+import UserList from './UserList';
+import Hoc from './HOC';
+
+const StocksData = [
+  {
+      id: 1,
+      name: 'TCS'
+        
+  },
+  {
+      id: 2,
+      name: 'Infosys'
+  },
+  {
+      id: 3,
+      name: 'Reliance'
+  }
+];
+const UsersData = [
+  {
+      id: 1,
+      name: 'Krunal'
+        
+  },
+  {
+      id: 2,
+      name: 'Ankit'
+  },
+  {
+      id: 3,
+      name: 'Rushabh'
+  }
+];
+
+const Stocks = Hoc(
+  StockList,
+  StocksData
+);
+
+const Users = Hoc(
+  UserList,
+  UsersData
+);
+
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div>
+        <Users></Users>
+      </div>
+    )
+  }
+}
+
+export default App;
