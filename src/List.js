180cd7f35c2d517c9e18073cec7af177d39ddaac
gunesh <gunesh26@gmail.com>
2022-06-08 22:55:34 +0530

    1 file modified

--- src/List.js
+++ src/List.js
@@ -1,20 +1,23 @@
-import React, { useEffect, useState } from "react"
-  
-function List({ getItems }){
-  
-    /* Initial state of the items */
-    const [items, setItems] = useState([]);
-  
-    /* This hook sets the value of items if 
+import React, { useEffect, useState } from 'react';
+
+function List({ getItems }) {
+  /* Initial state of the items */
+  const [items, setItems] = useState([]);
+
+  /* This hook sets the value of items if 
        getItems object changes */
-    useEffect(() => {
-        console.log("Fetching items");
-        setItems(getItems());
-    }, [getItems]);
-  
-    /* Maps the items to a list */
-    return <div>
-        {items.map(item => <div key={item}>{item}</div>)}
+  useEffect(() => {
+    console.log('Fetching items');
+    setItems(getItems());
+  }, [getItems]);
+
+  /* Maps the items to a list */
+  return (
+    <div>
+      {items.map((item) => (
+        <div key={item}>{item}</div>
+      ))}
     </div>
+  );
 }
 export default List;
