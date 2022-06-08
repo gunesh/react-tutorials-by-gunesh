cf129c68c55a67e9c0ed857e812c89179b45b025
gunesh <gunesh26@gmail.com>
2022-06-08 22:14:06 +0530

    1 file modified

--- src/StockList.js
+++ src/StockList.js
@@ -1,32 +1,26 @@
 import React, { Component } from 'react';
 import TableRow from './TableRow';
 
-class StockList extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  tabRow() {
-    if (this.props.data instanceof Array) {
-      return this.props.data.map(function (object, i) {
+const StockList = (props) => {
+  const tabRow = () => {
+    if (props.data instanceof Array) {
+      return props.data.map(function (object, i) {
         return <TableRow obj={object} key={i} />;
       });
     }
-  }
-  render() {
-    return (
-      <div className="container">
-        <table className="table table-striped">
-          <thead>
-            <tr>
-              <td>Stock Name</td>
-              <td>Stock Price</td>
-            </tr>
-          </thead>
-          <tbody>{this.tabRow()}</tbody>
-        </table>
-      </div>
-    );
-  }
-}
+  };
+  return (
+    <div className="container">
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <td>Stock Name</td>
+            <td>Stock Price</td>
+          </tr>
+        </thead>
+        <tbody>{tabRow()}</tbody>
+      </table>
+    </div>
+  );
+};
 export default StockList;
