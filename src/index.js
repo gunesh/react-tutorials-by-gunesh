5f386b4ddc0a1b6cfe3db8251c05fe2b62f25d1b
gunesh <gunesh26@gmail.com>
2022-06-22 22:00:55 +0530

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-import React, { StrictMode } from 'react';
+import React, { useState, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
-
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
+const App = () => {
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    var data = window.localStorage.getItem('SHOW_TOGGLE');
+    setShow(JSON.parse(data));
+  }, []);
+
+  useEffect(() => {
+    window.localStorage.setItem('SHOW_TOGGLE', JSON.stringify(show));
+  }, [show]);
+
+  return (
+    <React.Fragment>
+      <a
+        onClick={() => {
+          setShow(!show);
+        }}
+        href="#"
+      >
+        Toggle
+      </a>
+      {show && <div>Show/Hide Me</div>}
+    </React.Fragment>
+  );
+};
 root.render(
   <div>
-    Hello
+    <App />
   </div>
 );
