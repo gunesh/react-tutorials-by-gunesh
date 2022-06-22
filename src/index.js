2e1b9ae01ac1125e5f281a4f0059c15a8dd5f418
gunesh <gunesh26@gmail.com>
2022-06-22 21:50:12 +0530

    2 files modified, 17 deleted

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import ExternalAppStore from './ExternalAppStore';   // External React Redux store
-import { store, App } from './onelinesetup';   // Functional component and store
-import AppClass from './class_base_cmp'  // Class based Approach
-import { ExternalStore } from './redux/store';
 
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
-  <Provider store={ExternalStore}>
-    <ExternalAppStore />  
-    {/* <App /> */}
-    {/* <AppClass /> */}
-  </Provider>
+  <div>
+    Hello
+  </div>
 );
