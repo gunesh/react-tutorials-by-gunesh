f3a91e5b637bb31fbb4bfdba99a3ae331ec0fd95
gunesh <gunesh26@gmail.com>
2022-06-23 15:50:22 +0530

    1 file modified, 1 added

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Captcha, MyCaptcha } from './captcha';
 import Hello from './mobx/Hello';
 import App2 from './mobx/App2';
+import ControlledPanelGroup from './panel'
 import Table from './mobx/table';
 import GoogleChart from './Charts/GoogleChart';
 import { MapContainer } from './GoogleMap';
@@ -12,7 +13,8 @@ function App() {
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
-      <App2 />
+      {/* <App2 /> */}
+      <ControlledPanelGroup />
     </div>
   );
 }
