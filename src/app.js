b9d6209fe7cca8afc26813e9d95133cb964b2972
gunesh <gunesh26@gmail.com>
2022-06-25 21:24:26 +0530

    2 files modified

--- src/app.js
+++ src/app.js
@@ -1,30 +1,29 @@
 import React from 'react';
-import { Captcha, MyCaptcha } from './captcha';
-import Hello from './mobx/Hello';
-import App2 from './mobx/App2';
-import ControlledPanelGroup from './panel'
-import Table from './mobx/table';
-import GoogleChart from './Charts/GoogleChart';
-import { MapContainer } from './GoogleMap';
-import { TableExample } from './Tables';
 
 function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <pre>
-      1. Using Functional Components and Hooks Instead of Classes
-      2. Avoid Using State (If Possible)
-      3. Organize Files Related to the Same Component in One Folder
-      4. Avoid Using Indexes as Key Props
-      5. Opt for Fragments Instead of Divs Where Possible
-      6. Follow Naming Conventions
-      7. Avoid Repetitive Code
-      8. Use Object Destructuring For Props
-      9. Dynamically Render Arrays Using Map
-      10. Write Tests for Each React Component
-      11. Use lazy-loading / code splitting
-</pre>
+      <h1>Best Practices!</h1>
+
+      <ul>
+        <li>Using Functional Components and Hooks Instead of Classes</li>
+        <li>Avoid Using State (If Possible) </li>
+        <li>Organize Files Related to the Same Component in One Folder</li>
+        <li>Avoid Using Indexes as Key Props</li>
+        <li>Opt for Fragments Instead of Divs Where Possible </li>
+        <li>Follow Naming Conventions</li>
+        <li>Avoid Repetitive Code</li>
+        <li>Use Object Destructuring For Props</li>
+        <li>Dynamically Render Arrays Using Map</li>
+        <li>Write Tests for Each React Component </li>
+        <li>Use lazy-loading / code splitting</li>
+        {/* 
+        <li>--</li>
+        <li>--</li>
+        <li>--</li>
+        <li>--</li>
+         */}
+      </ul>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './app';
+import App from './app';
 
 const rootElement = document.getElementById('root');
 
-ReactDOM.render(<h1>Hello, from Guru99 Tutorials!</h1>, rootElement);
+ReactDOM.render(<App />, rootElement);
