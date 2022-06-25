ff8350c5097641721af2436f13746c50d6dfe1c5
gunesh <gunesh26@gmail.com>
2022-06-25 21:18:14 +0530

    2 files modified

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
-1. Using Functional Components and Hooks Instead of Classes
-2. Avoid Using State (If Possible)
-3. Organize Files Related to the Same Component in One Folder
-4. Avoid Using Indexes as Key Props
-5. Opt for Fragments Instead of Divs Where Possible
-6. Follow Naming Conventions
-7. Avoid Repetitive Code
-8. Use Object Destructuring For Props
-9. Dynamically Render Arrays Using Map
-10. Write Tests for Each React Component
-11. Use lazy-loading / code splitting
+# react-gunesh-jdgqss
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-gunesh-jdgqss)
--- src/app.js
+++ src/app.js
@@ -12,9 +12,19 @@ function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-      {/* <App2 /> */}
-      <ControlledPanelGroup />
+      <pre>
+      1. Using Functional Components and Hooks Instead of Classes
+      2. Avoid Using State (If Possible)
+      3. Organize Files Related to the Same Component in One Folder
+      4. Avoid Using Indexes as Key Props
+      5. Opt for Fragments Instead of Divs Where Possible
+      6. Follow Naming Conventions
+      7. Avoid Repetitive Code
+      8. Use Object Destructuring For Props
+      9. Dynamically Render Arrays Using Map
+      10. Write Tests for Each React Component
+      11. Use lazy-loading / code splitting
+</pre>
     </div>
   );
 }
