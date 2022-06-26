6e638c3355d166b6a173ddb795d1ffe349386ef8
gunesh <gunesh26@gmail.com>
2022-06-26 22:34:42 +0530

    1 file modified

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <p>Start editing to see some magic happen  :)</p>
     </div>
   );
 }
