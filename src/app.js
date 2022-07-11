57cc7351456ecfdce8edb596d23632ddc56bb0c5
gunesh <gunesh26@gmail.com>
2022-07-11 23:02:11 +0530

    1 file modified

--- src/app.js
+++ src/app.js
@@ -15,9 +15,7 @@ function App() {
   };
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-
+      
       <div>
         <Button
           color="danger"
