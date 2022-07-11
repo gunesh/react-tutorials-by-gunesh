2244850976568ad50640f9849a179d9976a0af5f
gunesh <gunesh26@gmail.com>
2022-07-11 22:47:32 +0530

    1 file modified

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Button } from 'reactstrap';
 
 function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen  :)</p>
+      <p>Start editing to see some magic happen :)</p>
+      <Button color="danger">Danger!</Button>
     </div>
   );
 }
