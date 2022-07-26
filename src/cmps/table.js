b51995264be7a21122bc54b0e078f78379b99f56
gunesh <gunesh26@gmail.com>
2022-07-26 15:27:24 +0530

    1 file modified

--- src/cmps/table.js
+++ src/cmps/table.js
@@ -1,36 +1,32 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import MaterialTable from "material-table";
+import React from 'react';
+import MaterialTable from 'material-table';
 
-class App extends Component {
-  render() {
-    return (
-      <div style={{ maxWidth: "100%" }}>
-        <MaterialTable
-          columns={[
-            { title: "Adı", field: "name" },
-            { title: "Soyadı", field: "surname" },
-            { title: "Doğum Yılı", field: "birthYear", type: "numeric" },
-            {
-              title: "Doğum Yeri",
-              field: "birthCity",
-              lookup: { 34: "İstanbul", 63: "Şanlıurfa" },
-            },
-          ]}
-          data={[
-            {
-              name: "Mehmet",
-              surname: "Baran",
-              birthYear: 1987,
-              birthCity: 63,
-            },
-          ]}
-          title="Demo Title"
-        />
-      </div>
-    );
-  }
-}
+const Example = (args) => {
+  return (
+    <div style={{ maxWidth: '100%' }}>
+      <MaterialTable
+        columns={[
+          { title: 'Adı', field: 'name' },
+          { title: 'Soyadı', field: 'surname' },
+          { title: 'Doğum Yılı', field: 'birthYear', type: 'numeric' },
+          {
+            title: 'Doğum Yeri',
+            field: 'birthCity',
+            lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+          },
+        ]}
+        data={[
+          {
+            name: 'Mehmet',
+            surname: 'Baran',
+            birthYear: 1987,
+            birthCity: 63,
+          },
+        ]}
+        title="Demo Title"
+      />
+    </div>
+  );
+};
 
-
-export default App
+export default Example;
